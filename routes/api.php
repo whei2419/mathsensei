2fc7d42314e9c6cf072834a1f7d3a449c79367f0
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-01-25 03:06:18 +0800

    basta eto structure ng question ko

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuestionController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,18 @@ use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::group(['prefix' => 'question'], function () {
+        Route::get('get', [QuestionController::class, 'get']);
+    });
+
+  
+
+
+
+
 });
 
 Route::post('/register', [AuthController::class, 'register']);
