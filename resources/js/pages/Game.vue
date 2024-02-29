dfe716cda52a84d8c00624e781dbfa6aef2b66ec
whei2419 <joelnacionales24@gmail.com>
2024-02-29 21:55:43 +0800

    fix result

--- resources/js/pages/Game.vue
+++ resources/js/pages/Game.vue
@@ -349,7 +349,7 @@ export default {
         data: {
           next: this.gameData.next,
           extra_time: this.gameData.extraTime,
-            sequence: this.sequence,
+            sequence: JSON.stringify( this.sequence),
           game_id: this.gameData.gameID,
           question_id: this.gameData.questionID,
           random_tap: this.gameData.randomTaps,
